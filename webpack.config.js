8138c13ef342e5ac3d3f07dcf5822fe2a692a323
Olivier Penhoat <openhoat@gmail.com>
2017-01-03 07:04:10 +0100

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = ({
   output: {
     path: path.resolve(__dirname, './dist/app'),
     filename: bundleName,
-    publicPath: '/assets/app'
+    publicPath: '/assets/app/'
   },
   module: {
     loaders: [
